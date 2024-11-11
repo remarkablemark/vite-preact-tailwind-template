82f4251999f647ecf198ac4088cab3a5222ef3e6
Mark <mark@remarkablemark.org>
2024-11-10 19:02:58 -0500

    feat: add tailwind

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,3 @@
-import './app.css';
-
 import { useState } from 'preact/hooks';
 import preactLogo from 'src/assets/preact.svg';
 
@@ -10,24 +8,43 @@ export default function App() {
 
   return (
     <>
-      <div>
+      <div className="flex justify-center">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
+          <img
+            src={viteLogo}
+            className="m-4 h-24 hover:drop-shadow-2xl"
+            alt="Vite logo"
+          />
         </a>
+
         <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+          <img
+            src={preactLogo}
+            className="m-4 h-24 hover:drop-shadow-2xl"
+            alt="Preact logo"
+          />
         </a>
       </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+
+      <h1 className="my-10 text-5xl font-bold">Vite + Preact + Tailwind</h1>
+
+      <div className="p-8">
+        <button
+          className="rounded-md border border-slate-300 bg-slate-50 px-4 py-2 text-center text-sm font-medium text-slate-800 shadow-sm transition-all hover:border-slate-800 focus:border-slate-800 focus:bg-slate-50 active:border-slate-800 active:bg-slate-50 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          onClick={() => setCount((count) => count + 1)}
+          type="button"
+        >
           count is {count}
         </button>
-        <p>
-          Edit <code>src/components/App/App.tsx</code> and save to test HMR
+
+        <p className="my-4 text-slate-600">
+          Edit{' '}
+          <code className="font-[monospace]">src/components/App/App.tsx</code>{' '}
+          and save to test HMR
         </p>
       </div>
-      <p class="read-the-docs">
+
+      <p className="text-slate-400">
         Click on the Vite and Preact logos to learn more
       </p>
     </>
